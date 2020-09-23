87b351c831104d5356758e4df57ce847cf776556
Will Shoemaker <shoemakah@gmail.com>
2020-09-23 01:53:45 -0700

    init recombination simulation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SLiM_recipes
 Playing around with SLiM
+
+To-do
+
+- get HGT working
+
+- figure out how to sample the same simulation at multiple timepoints
---
+++ run_slim.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+
+slim_script = 'test_recombination.slim'
+
+if os.geteuid() == 501:
+    directory = os.path.expanduser("~/GitHub/slim_recipes/")
+
+else:
+    directory = os.path.expanduser("/u/project/ngarud/wrshoema/slim_recipes/")
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--generations', required=True, type=int)
+parser.add_argument('--genome_size', required=True, type=int)
+parser.add_argument('--population_size', required=True, type=int)
+
+parser.add_argument('--per_base_mutation_rate', required=False, type=float, default=1e-6)
+parser.add_argument('--per_base_recombination_rate', required=False, type=float, default=1e-6)
+
+parser.add_argument('--beneficial_selection_coefficient', required=False, type=float, default=0.001)
+parser.add_argument('--deleterious_selection_coefficient', required=False, type=float, default=-0.001)
+
+parser.add_argument('--proportion_beneficial_sites', required=False, type=float, default=0)
+parser.add_argument('--proportion_deleterious_sites', required=False, type=float, default=0)
+parser.add_argument('--proportion_neutral_sites', required=False, type=float, default=1)
+
+
+args = parser.parse_args()
+
+generations = args.generations
+population_size = args.population_size
+genome_size = args.genome_size
+per_base_u = args.per_base_mutation_rate
+per_base_r = args.per_base_recombination_rate
+
+beneficial_s = args.beneficial_selection_coefficient
+deleterious_s = args.deleterious_selection_coefficient
+
+proportion_b = args.proportion_beneficial_sites
+proportion_d = args.proportion_deleterious_sites
+proportion_n = args.proportion_neutral_sites
+
+# extremely annoying, but to use % syntax with slim you have to have the outfile  in double parentheses and then single parentheses
+file_out = "'%stest_out2.txt'" % directory
+
+slim_line = 'slim -d generations=%d -d population_size=%d -d genome_size=%d -d per_base_u=%f -d per_base_r=%f -d beneficial_s=%f -d deleterious_s=%f -d proportion_b=%f -d proportion_d=%f -d proportion_n=%f -d "file_out=%s" %stest_recombination.slim' % (generations, population_size, genome_size, per_base_u, per_base_r, beneficial_s, deleterious_s, proportion_b, proportion_d, proportion_n, file_out, directory)
+#slim_line = 'slim -d generations=%d -d population_size=%d -d genome_size=%d -d per_base_u=%f -d per_base_r=%f -d beneficial_s=%f -d deleterious_s=%f -d proportion_b=%f -d proportion_d=%f -d proportion_n=%f %stest_recombination.slim' % (generations, population_size, genome_size, per_base_u, per_base_r, beneficial_s, deleterious_s, proportion_b, proportion_d, proportion_n, directory)
+
+
+print(slim_line)
+
+os.system(slim_line)
+
+#for s in [args.s]:
+#    f = '%stest_file.txt' % directory
+    #os.system('slim -d L=10000 -d u={}  -d r={} -d N={} -d s_b={} -d "fname=\'{}\'" -d nbases={} ~/project-ngarud/code/slim/{}.slim'.
+    #          format(mu, r, ne, s, f, nbases, args.f))
+
+    #os.system('slim -d L=%d '  )
+
+    #          '%s%s' % (directory, slim_script)
+
+
+
+
+# mutations info to parse output
+
+#0: within-file numeric identifier
+#1: Mutation id property, a within-run unique identifier for mutations that does not change over time
+#2: mutation type
+#3: chromosome position, zero-based
+#4: selection coefficient
+#5: dominance coefficient
+#6: subpopulation ID
+#7 generation where it originated
+#8: mutation prevalence=count of the number of times that it occurs in any genome in the population.
